dc76329966c0465358db7a2cb7f340b6d9ee3cba
Stefan Stan <stefan.stan93@yahoo.ro>
2016-05-09 14:48:27 +0300

    backend password encoding matching done

--- src/main/java/base/users/UserDao.java
+++ src/main/java/base/users/UserDao.java
@@ -9,11 +9,11 @@ public class UserDao {
 	public User getByLogin(String login) {
 		switch (login) {
 			case "admin":
-				return new User("admin", "Administrator", "admin", "ADMIN");
+				return new User("admin", "Administrator", "$2a$10$MJCv2bPyCLj2ZPbUuWzh8uCW4U1uqM/kyLpFV22W185XU45QVvDBa", "ADMIN");
 			case "special":
-				return new User("special", "Special Expert", "special", "ROLE_SPECIAL");
+				return new User("special", "Special Expert", "$2a$10$L7bMvpuHdtTN/qZ15XSje.rgwzBMcODZcnPg3c1mMgRQQzGSuatTG", "ROLE_SPECIAL");
 			case "user1":
-				return new User("user", "User Uno", "user");
+				return new User("user", "User Uno", "$2a$10$hF8nP6vt4Zhcr5whAZZWZ.bSI3oxSRQ8R2YeBkeaSW7IIGH.2cAIS");
 			default:
 				return null;
 		}
