771dc7b759787178065fc0c80902d724bae28ae0
Stefan Stan <stefan.stan93@yahoo.ro>
2016-05-10 16:03:37 +0300

    front end done

--- src/main/java/base/users/UserDao.java
+++ src/main/java/base/users/UserDao.java
@@ -12,8 +12,6 @@ public class UserDao {
 				return new User("admin", "Administrator", "$2a$10$MJCv2bPyCLj2ZPbUuWzh8uCW4U1uqM/kyLpFV22W185XU45QVvDBa", "ADMIN");
 			case "special":
 				return new User("special", "Special Expert", "$2a$10$L7bMvpuHdtTN/qZ15XSje.rgwzBMcODZcnPg3c1mMgRQQzGSuatTG", "ROLE_SPECIAL");
-			case "user1":
-				return new User("user", "User Uno", "$2a$10$hF8nP6vt4Zhcr5whAZZWZ.bSI3oxSRQ8R2YeBkeaSW7IIGH.2cAIS");
 			default:
 				return null;
 		}
